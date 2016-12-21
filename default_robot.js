06ee5270c0c7cc845cad2c64252b867403e3ba27
Wisbell <westleyisbell@gmail.com>
2016-12-20 21:59:01 -0600

    Added basic navbar and began setting up robot object

--- default_robot.js
+++ default_robot.js
@@ -1,3 +1,38 @@
-var robot = {
+var RobotWarrior = {};
 
+
+RobotWarrior.Robot.DefaultPlayer = function() {
+    this.name;
+    this.type;
+    this.health;
+    this.damage;
 }
+
+
+/*
+
+Robot Ideas
+
+westworld robots
+short circuit johnny 5
+
+The Matrix - flying robots
+
+I, Robot
+
+Transformers
+
+http://www.slashfilm.com/25-best-movie-robots/5/
+
+Terminator Salvation
+
+star wars c3p0   r2d2
+
+
+Chappie
+
+
+
+
+
+*/
